36253c09000103dd7cf0a917643772acdddcdd1c
ZaymonFC <generation281@gmail.com>
2018-08-21 14:18:48 +1000

    Fixed test compiling and handled asynchronous file test

--- server/data-access/file-handler.js
+++ server/data-access/file-handler.js
@@ -8,14 +8,14 @@ export default function makeFileHandler(fileName) {
   })
 
   function checkFileStatus() {
-    fs.access(fileName, fs.constants.F_OK | fs.constants.W_OK, (err) => {
-      if (err) {
-        console.error(`${file} ${err.code === 'ENOENT' ? 'does not exist' : 'is read-only'}`)
-        return false
-      } else {
-        console.log(`${file} exists, and it is writable`)
-        return true
-      }
+    return new Promise((resolve, reject) => {
+      fs.access(fileName, fs.constants.F_OK | fs.constants.W_OK, (err) => {
+        if (err) {
+          reject(`${file} ${err.code === 'ENOENT' ? 'does not exist' : 'is read-only'}`)
+        } else {
+          resolve(`${file} exists, and it is writable`)
+        }
+      })
     })
   }
 
--- server/test/data-access-test.spec.js
+++ server/test/data-access-test.spec.js
@@ -1,21 +1,25 @@
-import * as testDataGenerator from './data-access.generator.spec'
-import makeFileHandler from '../data-access/file-handler'
+import * as testDataGenerator from "./data-access.generator.spec";
+import makeFileHandler from "../data-access/file-handler";
 
-const assert = require('chai').assert
+const assert = require("chai").assert;
 
-const expectedJSON = testDataGenerator.getExampleJSON()
+const expectedJSON = testDataGenerator.getExampleJSON();
 
-describe('data-reader', () => {
-    it('should be invalid if file unavailable', () => {
-        let fileHandler = makeFileHandler('unavailable.json')
+describe("data-reader", () => {
+  it("should be invalid if file unavailable", () => {
+    let fileHandler = makeFileHandler("unavailable.json");
+    fileHandler.checkFileStatus()
+      .then(function(result) {
+        // Should not be called
+      },
+      function(err) {
+        assert.isNotNull(err)
+        console.log(err)
+      })
+  })
 
-        assert.isFalse(fileHandler.checkFileStatus())
-    })
-
-    // it('should read in file as JSON', () => {
-    //     const loadedJson = LoadFileContents('testdata/loadTest.json')
-    //     assert.equal(loadedJson, expectedJSON)
-    // })
-
-    
-})
+  // it('should read in file as JSON', () => {
+  //     const loadedJson = LoadFileContents('testdata/loadTest.json')
+  //     assert.equal(loadedJson, expectedJSON)
+  // })
+});
